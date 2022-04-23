5b8aa238c21dafd3639d01100f4cd7ac9c42a970
Himanshu Kumar <himanshurhlk57@gmail.com>
2022-04-23 16:05:10 +0530

    changes made

--- backend/routes/AllUsers.js
+++ backend/routes/AllUsers.js
@@ -1,21 +1,19 @@
-const express = require('express')
-const Router = express.Router()
-const User = require('../models/User')
+const express = require("express");
+const Router = express.Router();
+const User = require("../models/User");
 
+Router.get("/:id", async (req, res) => {
+  const users = await User.find({ _id: { $ne: req.params.id } });
 
-Router.get('/:id',async(req,res)=>{
-    const users = await User.find({_id: {$ne: req.params.id} })
+  res.json(users);
+});
 
-    res.json(users)
-})
+Router.get("/:id1/:id2", async (req, res) => {
+  const users = await User.find({
+    _id: { $nin: [req.params.id1, req.params.id2] },
+  });
 
+  res.json(users);
+});
 
-Router.get('/:id1/:id2',async(req,res)=>{
-    const users = await User.find({_id: {$nin:[req.params.id1,req.params.id2] }})
-
-    res.json(users);
-})
-
-
-
-module.exports = Router
+module.exports = Router;
--- backend/routes/google-oauths.js
+++ backend/routes/google-oauths.js
@@ -1,63 +1,60 @@
-const mongoose = require('mongoose')
-const express = require('express')
+const mongoose = require("mongoose");
+const express = require("express");
 const Router = express.Router();
-const dotenv = require('dotenv')
-const {OAuth2Client} = require('google-auth-library')
-const Login = require('../models/google-oauth')
-const User = require('../models/User')
+const dotenv = require("dotenv");
+const { OAuth2Client } = require("google-auth-library");
+const Login = require("../models/google-oauth");
+const User = require("../models/User");
 
 dotenv.config();
 const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID);
 
+Router.post("/", async (req, res) => {
+  const { token } = req.body;
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+  });
 
-Router.post('/', async (req, res) => {
-    const { token } = req.body;
-    const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+  const { name, email, picture } = ticket.getPayload();
+
+  const splitData = name.split(" ");
+
+  let loginuser = await Login.findOne({ email: email });
+
+  let user = await User.findOne({ email: email });
+
+  let id = null;
+
+  if (!loginuser && !user) {
+    loginuser = new Login({
+      name: name,
+      email: email,
+      picture: picture,
     });
 
-    const { name, email, picture } = ticket.getPayload();
+    user = new User({
+      firstName: splitData[0],
+      lastName: splitData[1],
+      email: email,
+      picture: picture,
+    });
 
-    const splitData = name.split(" ");
+    const result1 = await loginuser.save();
+    const result2 = await user.save();
 
+    id = result2._id;
 
-    let loginuser = await Login.findOne({ email: email })
+    console.log(result1);
+    console.log(result2);
+  }
 
-    let user = await User.findOne({ email: email })
+  if (!id) {
+    id = user._id;
+  }
 
-    let id = null;
-
-    if (!loginuser && !user) {
-        loginuser = new Login({
-            name: name,
-            email: email,
-            picture: picture
-        })
-
-        user = new User({
-            firstName: splitData[0],
-            lastName: splitData[1],
-            email: email,
-            picture: picture
-        })
-
-        const result1 = await loginuser.save();
-        const result2 = await user.save();
-
-        id = result2._id;
-
-        console.log(result1);
-        console.log(result2);
-    }
-
-    if(!id){
-        id = user._id;
-    }
-
-    res.status(201);
-    res.json({id,email,name,picture});
+  res.status(201);
+  res.json({ id, email, name, picture });
 });
 
-
-module.exports = Router
+module.exports = Router;
